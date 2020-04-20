06d96df413e90c14de7653238f07342fd12ede1c
JoseGrn <59964334+JoseGrn@users.noreply.github.com>
2020-04-19 20:33:54 -0600

    subida tabla hash y cola

--- Clases/TareaCola.cs
+++ Clases/TareaCola.cs
@@ -15,5 +15,15 @@ namespace EstructuraDato_Lab04.Clases
         {
             return Far1.Prioridad.CompareTo(Far2.Prioridad);
         };
+
+        public Comparison<TareaCola> CompararTitulo = delegate (TareaCola Far1, TareaCola Far2)
+        {
+            return Far1.Titulo.CompareTo(Far2.Titulo);
+        };
+
+        public Comparison<TareaCola> CompararID = delegate (TareaCola Far1, TareaCola Far2)
+        {
+            return Far1.Titulo.CompareTo(Far2.Titulo);
+        };
     }
 }
